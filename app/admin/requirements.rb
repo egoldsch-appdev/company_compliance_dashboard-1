a44acd0fde07e3cd6860586d9fee3596740fea45
Raghu Betina <raghu@firstdraft.com>
2018-08-11 03:36:07 +0000

    Whitelist columns in Requirement for ActiveAdmin

--- app/admin/requirements.rb
+++ app/admin/requirements.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Requirement do
+
+ permit_params :employee_id, :department_id, :elearning_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
