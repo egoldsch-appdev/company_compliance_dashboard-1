3d6a315e6caf71d33c8f87c321706a7fea349e89
Raghu Betina <raghu@firstdraft.com>
2018-08-11 03:35:10 +0000

    Generate departments resource with starter:resource

---
+++ app/controllers/departments_controller.rb
@@ -0,0 +1,81 @@
+class DepartmentsController < ApplicationController
+  def index
+    @departments = Department.all
+
+    render("departments/index.html.erb")
+  end
+
+  def show
+    @department = Department.find(params[:id])
+
+    render("departments/show.html.erb")
+  end
+
+  def new
+    @department = Department.new
+
+    render("departments/new.html.erb")
+  end
+
+  def create
+    @department = Department.new
+
+    @department.name = params[:name]
+    @department.mcm_id = params[:mcm_id]
+
+    save_status = @department.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/departments/new", "/create_department"
+        redirect_to("/departments")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Department created successfully.")
+      end
+    else
+      render("departments/new.html.erb")
+    end
+  end
+
+  def edit
+    @department = Department.find(params[:id])
+
+    render("departments/edit.html.erb")
+  end
+
+  def update
+    @department = Department.find(params[:id])
+
+    @department.name = params[:name]
+    @department.mcm_id = params[:mcm_id]
+
+    save_status = @department.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/departments/#{@department.id}/edit", "/update_department"
+        redirect_to("/departments/#{@department.id}", :notice => "Department updated successfully.")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Department updated successfully.")
+      end
+    else
+      render("departments/edit.html.erb")
+    end
+  end
+
+  def destroy
+    @department = Department.find(params[:id])
+
+    @department.destroy
+
+    if URI(request.referer).path == "/departments/#{@department.id}"
+      redirect_to("/", :notice => "Department deleted.")
+    else
+      redirect_back(:fallback_location => "/", :notice => "Department deleted.")
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  # Routes for the Department resource:
+  # CREATE
+  get "/departments/new", :controller => "departments", :action => "new"
+  post "/create_department", :controller => "departments", :action => "create"
+
+  # READ
+  get "/departments", :controller => "departments", :action => "index"
+  get "/departments/:id", :controller => "departments", :action => "show"
+
+  # UPDATE
+  get "/departments/:id/edit", :controller => "departments", :action => "edit"
+  post "/update_department/:id", :controller => "departments", :action => "update"
+
+  # DELETE
+  get "/delete_department/:id", :controller => "departments", :action => "destroy"
+  #------------------------------
+
   # Routes for the Employee resource:
   # CREATE
   get "/employees/new", :controller => "employees", :action => "new"
